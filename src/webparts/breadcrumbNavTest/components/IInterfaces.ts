cd6aae34c9ec5697d664021e5d9db3c914876fcb
Francesco Meloni <f.meloni@avanade.com>
2020-08-03 14:04:37 +0000

    add IDetailsListDocumentItem + IDetailsListContent

--- src/webparts/breadcrumbNavTest/components/IInterfaces.ts
+++ src/webparts/breadcrumbNavTest/components/IInterfaces.ts
@@ -1,7 +1,23 @@
+import { IContentTypeInfo } from "@pnp/sp/presets/all";
+
+export interface IDetailsListContent {
+  type: string;
+  content: IDetailsListBasicItem[] | IDetailsListDocumentItem[];
+}
+
 export interface IDetailsListBasicItem {
   key: number;
   name: string;
   value: string;
+}
+
+export interface IDetailsListDocumentItem {
+  id: number;
+  title: string;
+  fieldStr1: string;
+  fieldStr2: string;
+  fieldDt3: Date;
+  fieldInt4: number;
 }
 
 export interface ISearchParams {
